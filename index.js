369691deb7fce366044742d90e5cc4bd29e6dd0c
JoeTodes <jtwilson.home@gmail.com>
2024-04-03 10:16:38 -0600

    tidied up

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 const { findMinMaxAvg } = require("./src/minmaxavg/minmaxavg.js");
-
 findMinMaxAvg(__dirname + "/src/minmaxavg/inputs/list4.txt");
+
+// const { readInput, sumWagons } = require("./src/expedition/expedition.js");
+// let inArr = readInput(__dirname + "/src/expedition/inputs/sample_input.txt");
+// sumWagons(inArr);
